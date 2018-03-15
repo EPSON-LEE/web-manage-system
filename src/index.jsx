6a8cc5ce5c80aa4555ef447b5cc226611d038074
leejiahao <491071291@qq.com>
2018-03-15 09:33:25 +0800

    json-server

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
 import { Router, Route, Link, hashHistory } from 'react-router'
 import routes from './modules/routes/routes.jsx'
 
 ReactDOM.render(
-  <Router history={hashHistory} routes={routes} />,
+  <Provider>
+    <Router history={hashHistory} routes={routes} />
+  </Provider>,
   document.getElementById('app')
 );
