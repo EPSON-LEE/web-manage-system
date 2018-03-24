3ab26d4f306fe4bd6a453ea0952ff98247a9100b
leejiahao <491071291@qq.com>
2018-03-25 01:45:21 +0800

    add redux-logger

--- src/modules/store.jsx
+++ src/modules/store.jsx
@@ -1,10 +1,14 @@
 import {applyMiddleware, createStore,combineReducers} from 'redux'
+import {createLogger} from 'redux-logger'
 import thunk from 'redux-thunk'
 import root from './containers/About/reducers.jsx'
+
+const logger = createLogger();
+
 
 const store = createStore(
     root,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, logger)
   )
 export default store
